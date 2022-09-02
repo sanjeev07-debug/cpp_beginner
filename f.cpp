769d00d00fa58392c592bcf7237e723c914e8530
Sanjeev varma <94509003+sanjeev07-debug@users.noreply.github.com>
2022-09-02 11:34:27 +0530

    Add files via upload

---
+++ f.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int test[3][2] = {{2,-3},{4,3},{9,1}};
+	for(int i = 0;i<3;i++) {
+		for(int j = 0;j<2;++j) {
+			cout<<i<<j<<endl;
+		}
+	}
+	return 0;
+}
